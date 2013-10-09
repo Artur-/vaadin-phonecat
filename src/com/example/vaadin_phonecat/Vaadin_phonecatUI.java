ff103c8c950a96c5ab03a67eb934297d7c86a932
Artur Signell <artur@vaadin.com>
2013-10-09 21:42:24 +0300

    Step 1

--- src/com/example/vaadin_phonecat/Vaadin_phonecatUI.java
+++ src/com/example/vaadin_phonecat/Vaadin_phonecatUI.java
@@ -6,6 +6,7 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
+import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Label;
@@ -21,8 +22,13 @@ public class Vaadin_phonecatUI extends UI {
 
 	@Override
 	protected void init(VaadinRequest request) {
-		Label text = new Label("Nothing here "+("yet"+"!"));
-		final VerticalLayout layout = new VerticalLayout(text);
+		Label nexus = new Label(
+				"Nexus S<br/>Fast just got faster with Nexus S.",
+				ContentMode.HTML);
+		Label motorola = new Label(
+				"Motorola XOOM™ with Wi-Fi<br/>The Next, Next Generation tablet.",
+				ContentMode.HTML);
+		final VerticalLayout layout = new VerticalLayout(nexus, motorola);
 		setContent(layout);
 	}
 
